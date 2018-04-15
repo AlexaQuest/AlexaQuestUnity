ae04acabb12e5359334612997f6a32922534a5d5
FwoopFwoop <zacharysamiyoussef@gmail.com>
2018-04-15 01:43:31 -0400

    Sword on the alexa now

---
+++ Assets/SwordListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameAPI;
+using UnityEngine;
+
+public class SwordListener : APIEventListener {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void HandleEvent(APIEventType apievent)
+	{
+		if (apievent == APIEventType.ATTACK)
+		{
+			transform.Translate(10, 0, 0);
+		}
+	}
+}
