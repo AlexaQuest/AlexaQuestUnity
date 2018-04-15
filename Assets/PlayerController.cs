60e7af2bae0e2803bdfa9205fb79b3274b725007
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2018-04-15 03:09:05 -0400

    animations

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using GameAPI;
+using UnityEngine;
+
+public class PlayerController : APIEventListener
+{
+	private Animator anim;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public override void HandleEvent(APIEventType apievent)
+	{
+		anim.SetTrigger(apievent.ToString().ToLower());
+	}
+}
